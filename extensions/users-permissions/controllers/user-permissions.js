91d628b744ab51fe98a8b0d495a4002c0b195795
Paula Nerenberg <paulita.glam@gmail.com>
2021-07-08 18:01:41 +0000

    Removed out-dated components

--- extensions/users-permissions/controllers/user-permissions.js
+++ extensions/users-permissions/controllers/user-permissions.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -6,15 +6,13 @@
  */
 
 module.exports = {
-    async me(ctx) {
-        const user = ctx.stat.user;
+  async me(ctx) {
+    const user = ctx.stat.user;
 
-        if (!uers){
-            return ctx.badRequest(null, [
-                {messages: [{id: "No authorization header was found"}]},
-            ]);
-        }
-
+    if (!uers) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
     }
-
+  },
 };
